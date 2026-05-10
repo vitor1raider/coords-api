887b1ada83e7046b4122fd1b8b2ed2eb63212b87
vitor1raider <vitorhdt.r@gmail.com>
2026-05-10 18:15:55 -0300

    first commit

---
+++ src/test/java/com/points/coords_api/CoordsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.points.coords_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoordsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
